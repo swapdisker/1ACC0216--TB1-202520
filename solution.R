8e653a445274c6a1238c2643123aa4b05cf52e58
yez <u202312907@upc.edu.pe>
2025-10-10 20:02:50 -0500

    Agregue los 4 primeros graficos

--- solution.R
+++ solution.R
@@ -1,3 +1,4 @@
+
 library(dplyr)
 library(ggplot2) # Nice heatmaps
 # Importamos las librerias necesarias
@@ -98,3 +99,98 @@ print(summary_after)
 write.csv(bookings_df_wins, "/home/cisco/Documents/Studies/uni-courses/FDS/positron/tb1-fds/hotel_bookings_winsorized.csv", row.names = FALSE)
 
 cat("\n Dataset con outliers tratados guardado exitosamente como 'hotel_bookings_winsorized.csv'\n")
+
+
+#======= VISUALIZACION DE LOS DATOS ========
+
+#====== GRAFICO 1 =======
+# Crear grafico mostrando la cantidad de reservas por tipo de hotel
+
+ggplot(bookings_df_wins, aes(x = hotel, fill = hotel)) +
+  geom_bar() +
+  labs(title = "Cantidad de reservas por tipo de hotel",
+       x = "Tipo de hotel",
+       y = "Número de reservas") +
+  theme_minimal() +
+  scale_fill_manual(values = c("steelblue", "orange"))
+
+
+#======== GRAFICO 2 ========
+#QUEREMOS SABER SI LA DEMANDA ESTA AUMENTANDO CON EL TIEMPO
+
+# Agrupar por año y mes de llegada
+demanda_mensual <- bookings_df_wins %>%
+  group_by(arrival_date_year, arrival_date_month) %>%
+  summarise(reservas = n(), .groups = 'drop')
+
+# Se convierte el nombre del mes a numero para ordenarlo luego
+demanda_mensual$arrival_date_month <- match(demanda_mensual$arrival_date_month, month.name)
+
+# Lo usaremos para mostrarlo en el eje x cronologicamente
+demanda_mensual <- demanda_mensual %>%
+  mutate(fecha = as.Date(paste(arrival_date_year, arrival_date_month, "01", sep = "-")))
+
+# Gráfico de línea temporal de la demanda mensual de todos los anios
+ggplot(demanda_mensual, aes(x = fecha, y = reservas, color = factor(arrival_date_year))) +
+  geom_line(size = 1) +
+  geom_smooth(aes(group = 1), method = "loess", color = "#6A0DAD", se = FALSE, size = 1.3) +
+  labs(title = "Evolución mensual de la demanda de reservas (2015–2017)",
+       x = "Fecha",
+       y = "Número de reservas",
+       color = "Año") +
+  theme_minimal()
+
+
+#======== GRAFICO 3 ===========
+#Grafico para saber las temporadas de reservas
+
+# Agrupar por año y mes
+heat_data <- bookings_df_wins %>%
+  group_by(arrival_date_year, arrival_date_month) %>%
+  summarise(reservas = n(), .groups = 'drop') %>%
+  mutate(arrival_date_month = factor(arrival_date_month, levels = month.name))
+
+# Calcular umbrales numéricos
+quantiles <- quantile(heat_data$reservas, probs = c(0.33, 0.66))
+quantiles
+
+heat_data <- heat_data %>%
+  mutate(temporada = case_when(
+    reservas <= quantiles[1] ~ "Baja",
+    reservas <= quantiles[2] ~ "Media",
+    TRUE ~ "Alta"
+  ))
+
+
+ggplot(heat_data, aes(x = arrival_date_month,
+                      y = factor(arrival_date_year),
+                      fill = temporada)) +
+  geom_tile(color = "white") +
+  scale_fill_manual(values = c("Baja" = "#AED6F1",
+                               "Media" = "#5DADE2",
+                               "Alta" = "#154360")) +
+  labs(title = "Mapa de calor de temporadas de reservas por mes y año",
+       x = "Mes",
+       y = "Año",
+       fill = "Temporada") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# ======== GRAFICO 4 ===========
+# Grafico para saber la duracion promedio de estadia por tipo de hotel
+
+
+# Crear variable de duración total
+bookings_df_wins <- bookings_df_wins %>%
+  mutate(duracion_total = stays_in_week_nights + stays_in_weekend_nights)
+
+# Crear gráfico boxplot
+ggplot(bookings_df_wins, aes(x = hotel, y = duracion_total, fill = hotel)) +
+  geom_boxplot(outlier.color = "red", outlier.size = 2, alpha = 0.7) +
+  scale_fill_manual(values = c("steelblue", "orange")) +
+  labs(title = "Distribución de la duración de las estancias por tipo de hotel",
+       x = "Tipo de hotel",
+       y = "Duración de la estancia (noches)") +
+  theme_minimal()
+
+
